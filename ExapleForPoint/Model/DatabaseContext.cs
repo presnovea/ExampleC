2bce644461c835bb1325b549a1950bcd4269e714
Ололой Ололоев <epresnov@live.com>
2021-05-14 19:50:29 +0300

    added DB EF connection

---
+++ ExapleForPoint/Model/DatabaseContext.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExapleForPoint.Modelling
+{
+    /// <summary>
+    /// ORM представление клиентов
+    /// </summary>
+    public class Customers
+    {
+        public decimal ID { get; set; }
+        public string LastName { get; set; }
+        public string FirstName { get; set; }
+        public string MiddleName { get; set; }
+        public string Sex { get; set; }
+        public DateTime BirthDate { get; set; }
+        public DateTime RegistrationDate { get; set; }
+
+        //public virtual ICollection<Orders> Ord { get; set; }
+    }
+    /// <summary>
+    /// ORM представление заказов
+    /// </summary>
+    public class Orders
+    {
+        public decimal ID { get; set; }
+        public decimal CustomerID { get; set; }
+        public DateTime OrderDate { get; set; }
+        public decimal Price { get; set; }
+
+        // public virtual Customers Customers { get; set; }
+    }
+
+    /// <summary>
+    /// Контекст БД
+    /// </summary>
+    public class PointExampleContext : DbContext
+    {
+        public PointExampleContext() : base(Util.EFConnectionString())
+        { }
+        //"name=PointDbString"
+
+        public DbSet<Customers> customers { get; set; }
+        public DbSet<Orders> orders { get; set; }
+    }
+
+   
+}
--- ExapleForPoint/Model/Model.cs
+++ ExapleForPoint/Model/Model.cs
@@ -1,12 +1,61 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Data.Entity;
+using System.Data.SqlClient;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace ExapleForPoint.Modelling
 {
-    class Model
+    public class DbParams
     {
+        private string dbName;
+        private string dataSource;
+        private string port;
+        private string userID;
+        private string password;
+
+        public string DbName { 
+            get { return dbName; }
+            set { dbName = value; }
+        }
+        public string DataSource
+        {
+            get { return dataSource; }
+            set { dataSource = value; }
+        }
+        public string Port
+        {
+            get { return port; }
+            set { port = value; }
+        }
+        public string UserID
+        {
+            get { return userID; }
+            set { userID = value; }
+        }
+        public string Password
+        {
+            get { return password; }
+            set { password = value; }
+        }
+
+    }
+
+    internal class DataBaseWorker
+    {
+        internal static string EFConnectionString (DbParams dbParams)
+        {
+            SqlConnectionStringBuilder cs = new SqlConnectionStringBuilder();
+            cs.DataSource = dbParams.DataSource + "," + dbParams.Port;
+            cs.InitialCatalog = dbParams.DbName;
+            cs.UserID = dbParams.UserID;
+            cs.Password = dbParams.Password;
+
+            cs.MultipleActiveResultSets = true;
+
+            return cs.ToString();
+        }
     }
 }
